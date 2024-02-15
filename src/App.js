b72d4f7050ad1b687b39493640969c7ea3036d95
allprotas <a.allouache@esi-sba.dz>
2024-02-15 16:33:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to me learning docker</h2>
+        </div>
+        <p className="App-intro">
+          This is Ayhem and ahmed and anes
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
